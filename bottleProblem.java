b88f1a1e9a207a5454bc00e145377de39e8bd669
naomital <37747268+naomital@users.noreply.github.com>
2019-06-03 23:15:36 +0300

    Update bottleProblem.java

--- bottleProblem.java
+++ bottleProblem.java
@@ -1,35 +1,69 @@
-public class bottleProblem{
 
-     public static void main(String []args){
-         boolean test [][]= bottleProblem(1,2);
-         for (int i=0;i<6;i++){
-        for(int j=0;j<6;j++){
-           System.out.println(test[i][j]);
+package Algo2;
 
-        }
-         }
-         
-     }
-public static boolean [][] bottleProblem(int n,int m){
-    int size= (n+1)*(m+1);
-    boolean matrix [][]=new boolean [size][size];
-    for (int i=0;i<size;i++){
-        for(int j=0;j<size;j++){
-         int row = findeIndex(i,j,m+1);
-         matrix[row][findeIndex(0,j,m+1)]=true;
-         matrix[row][findeIndex(i,0,m+1)]=true;
-         matrix[row][findeIndex(i,m,m+1)]=true;
-         matrix[row][findeIndex(n,j,m+1)]=true;
-         matrix[row][findeIndex(i+j-Math.min(i+j,n),Math.min(i+j,n),m+1)]=true;
-         matrix[row][findeIndex(Math.min(i+j,m),i+j - Math.min(i+j,m),m+1)]=true;
+import java.util.Arrays;
 
-         
-        }
-        
+
+public class Bottle_FW {
+
+    public static int findIndex(int i, int j, int q) {
+        return i * q + j;
     }
-    return matrix;
-}
-public static int findeIndex(int i,int j, int p){
-    return p*i+j;
-}
+
+    
+    public static boolean[][] BuildMat(int n, int m) {
+        int size = (n + 1) * (m + 1);
+        boolean[][] mat = new boolean[size][size];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                int row = findIndex(i, j, m + 1);
+                mat[row][findIndex(0, j, m + 1)] = true;
+                mat[row][findIndex(i, 0, m + 1)] = true;
+                mat[row][findIndex(i, m, m + 1)] = true;
+                mat[row][findIndex(n, j, m + 1)] = true;
+                mat[row][findIndex(Math.max(0, i + j - m), Math.min(i + j, m), m + 1)] = true;
+                mat[row][findIndex(Math.min(i + j, n), Math.max(0, i + j - n), m + 1)] = true;
+            }
+        }
+        return mat;
+    }
+
+    public static void FloydW(boolean[][] mat) {
+        int n = mat.length;
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    mat[i][j] = (mat[i][j] || (mat[i][k] && mat[k][j]));
+                }
+            }
+        }
+    }
+    
+    public static int numOfTiesElement(boolean [][] mat){
+        int num=0;
+        int [] c=new int[mat.length];
+        for (int i = 0; i < mat.length; i++) {
+            if(c[i]==0){
+                num++;
+                c[i]=num;
+            }
+            for (int j = 0; j < mat.length; j++) {
+                if(c[j]==0 && mat[i][j]) c[j]=num;
+            }
+        }
+        return num;
+    }
+    
+    public static void main(String[] args) {
+        boolean [][] mat=BuildMat(1, 2);
+        for (int i = 0; i < mat.length; i++) {
+            System.out.println(Arrays.toString(mat[i]));
+        }
+        FloydW(mat);
+        System.out.println("//////////////////");
+        for (int i = 0; i < mat.length; i++) {
+            System.out.println(Arrays.toString(mat[i]));
+        }
+        System.out.println("num of ties element: "+numOfTiesElement(mat));
+    }
 }
